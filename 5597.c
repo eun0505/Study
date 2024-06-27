73789a6d5837b30641d13461e47cd89da6be3e8a
eun0505 <cwy0505@naver.com>
2024-06-27 16:59:50 +0900

    modified:   5597.c

--- 5597.c
+++ 5597.c
@@ -1,1 +1,18 @@
 // test
+#include <stdio.h>
+
+int main() {
+    int check[31];
+    int check_log[31];
+    for(int i=1; i<= 30; i++) {
+        check[i] = i;
+    }
+    for(int j=0; j<28; j++) {
+        scanf("%d", &check_log[j]);
+        check[check_log[j]] = 0;
+    }
+    for(int i=1; i<= 30; i++) {
+        if(check[i] != 0)
+            printf("%d\n", i);
+    }
+}
